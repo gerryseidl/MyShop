daba5246360281bd7792bbc6553a2203bb44fcde
gerryseidl <49313877+gerryseidl@users.noreply.github.com>
2019-04-11 14:38:42 -0400

    updated

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,26 @@ namespace MyShop.WebUI.Controllers
             context = productContext;
             productCategories = productCategoryContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList(); // get a list of products
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            { // if a category is not passed in, use all products
+                products = context.Collection().ToList();
+            }
+            else
+            { // if a category was passed in, filter products on category
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
         public ActionResult Details(string Id)
         {
